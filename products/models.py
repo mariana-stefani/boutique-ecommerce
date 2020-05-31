80489bdd3a78d11ba0e8cfdb536219f3686530ff
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-05-31 15:54:35 +0100

    Added a string method to the Category class on products/models.py

--- products/models.py
+++ products/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
