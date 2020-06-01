108adfa20e34bf194d19179b9a549a6deb2d21bd
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-01 18:14:34 +0100

    Imported get_object_or_404 on bag/context.py

--- bag/context.py
+++ bag/context.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from products.models import Product
+
 
 def bag_contents(request):
 
@@ -10,14 +12,14 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
-    product = get_object_or_404(Product, pk=item_id)
-    total += quantity * product.price
-    product_count += quantity
-    bag_items.append({
-        'item_id': item_id,
-        'quantity': quantity,
-        'product': product,
-    })
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
