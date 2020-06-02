b6c714a5e0d008ab6fd006fafd6c9f012c210ba0
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-02 15:07:38 +0100

    Added css to style the notifications, on base.css

--- bag/views.py
+++ bag/views.py
@@ -79,6 +79,7 @@ def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
