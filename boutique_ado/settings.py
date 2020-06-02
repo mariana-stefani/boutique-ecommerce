f9a8720742afb41eb7e1779096ae75a3a713b7e5
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-02 17:13:04 +0100

    Added builtins to templates on settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -80,6 +80,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.context.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ],
         },
     },
 ]
