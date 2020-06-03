2d221a988e12792fc7e2195381016fbc4034100a
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-03 18:06:59 +0100

    Updated checkout view function with else statement on checkout/views.py

--- checkout/views.py
+++ checkout/views.py
@@ -61,6 +61,9 @@ def checkout(request):
 
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
